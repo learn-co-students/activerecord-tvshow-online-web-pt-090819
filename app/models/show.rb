db31ac45efe0519b55168dd880ec3ccb52c95f2a
Aschonely <aschonely@gmail.com>
2019-11-23 17:12:38 -0500

    Done.

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,30 +1,30 @@
 class Show < ActiveRecord::Base
+  
+  def self.highest_rating
+    self.maximum(:rating)
+  end
 
-  def highest_rating
-   
+  def self.most_popular_show
+    self.where("rating = ?", self.highest_rating).first
+  end
+
+  def self.lowest_rating
+    self.minimum(:rating)
+  end
+
+  def self.least_popular_show
+    self.where("rating = ?", self.lowest_rating).first
+  end
+
+  def self.ratings_sum
+    self.sum(:rating)
   end
   
-  def most_popular_show
-
+  def self.popular_shows
+    self.where("rating >= ?", 5)
   end
-  
-  def lowest_rating
 
-  end
-  
-  def least_popular_show
-
-  end
-  
-  def ratings_sum
-
-  end
-  
-  def popular_shows
-
-  end
-  
-  def shows_by_alphabetical_order
-
+  def self.shows_by_alphabetical_order
+    self.order(:name)
   end
 end
--- db/migrate/001_create_shows.rb
+++ db/migrate/001_create_shows.rb
@@ -1,10 +1,10 @@
-class Createshows < ActiveRecord::Migration[5.2]
-  def change
-    create_table :shows do |t|
-      t.string :name
-      t.string :day
-      t.string :network
-      t.integer :rating
-    end  
-  end  
-end
+class CreateShows < ActiveRecord::Migration[5.2]
+    def change
+      create_table :shows do |t|
+        t.string :name
+        t.string :day
+        t.string :network
+        t.integer :rating
+      end
+    end
+  end
